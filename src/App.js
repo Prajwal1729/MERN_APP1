0ed97c2d829208e8c92ee13224263d812bac1dc1
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2026-03-21 11:50:12 +0530

    leftmenu changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes,Route } from 'react-router-dom';
 //import routes here //
 import Login from './components/Login';
 import CreateAccount from './components/CreateAccount';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/admin/login' element = { <Login/>} />
         <Route path='/admin/createaccount' element= { <CreateAccount/> } />
+        <Route path='/admin/dashboard' element={<Dashboard/>} />
       </Routes>
     
       
